62b7e9a479fcc9df23253acf778ac3402b79d133
MansiMantri <mansimantri902@gmail.com>
2025-12-11 15:12:13 -0500

    chnage gemini model

--- data_processing.py
+++ data_processing.py
@@ -77,7 +77,7 @@ Return response strictly in this JSON format:
   "summary": [...]
 }}
             """
-            model = genai.GenerativeModel("gemini-1.5-flash")
+            model = genai.GenerativeModel("gemini-1.5-flash-latest")
             response = model.generate_content(
                 prompt,
                 generation_config=genai.types.GenerationConfig(
